53be6d1551751e66fd2e5efa186475a2416b31d3
Glynn Bird <glynn.bird@gmail.com>
2025-11-24 14:07:44 +0000

    hide buttons until ready

--- frontend/app/pages/note/[id].vue
+++ frontend/app/pages/note/[id].vue
@@ -4,6 +4,7 @@ import { parseMarkdown } from '@nuxtjs/mdc/runtime'
 const { getNoteFromAPI, getNoteFromCache, currentNote, deleteNote } = useNotesList()
 const route = useRoute()
 const id = route.params.id
+const loaded = ref(false)
 
 // try loading from cache first
 const ast = ref(null)
@@ -14,6 +15,7 @@ getNoteFromCache(id)
 setTimeout(async function() {
   await getNoteFromAPI(id)
   ast.value =  await parseMarkdown(currentNote.value.body)
+  loaded.value = true
 }, 1)
 
 async function deleteThisNote() {
@@ -31,6 +33,8 @@ async function edit() {
 </style>
 <template>
   <MDCRenderer v-if="ast?.body" class="markdown-body" :body="ast.body" :data="ast.data" />
-  <v-btn color="secondary" @click="edit">Edit</v-btn>
-  <v-btn color="error" @click="deleteThisNote">Delete</v-btn>
+  <div v-if="loaded">
+    <v-btn color="secondary" @click="edit">Edit</v-btn>
+    <v-btn color="error" @click="deleteThisNote">Delete</v-btn>
+  </div>
 </template>

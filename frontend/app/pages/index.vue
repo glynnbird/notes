9f56d99bab1d4479cfbd59d62b89fb9252e84869
Glynn Bird <glynn.bird@gmail.com>
2025-11-27 13:24:09 +0000

    added lastEdited to list

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,5 +1,11 @@
 <script setup>
   const { recentNotes, resetCurrentNote } = useNotesList()
+
+  import TimeAgo from 'javascript-time-ago'
+  import en from 'javascript-time-ago/locale/en'
+  TimeAgo.addDefaultLocale(en)
+  const timeAgo = new TimeAgo('en-GB')
+
   resetCurrentNote()
 </script>
 <template>
@@ -7,6 +13,10 @@
   <PWARefresh></PWARefresh>
 
   <v-list>
-    <v-list-item v-for="note in recentNotes" :title="note.title" :to="`/note/${note.id}`"></v-list-item>
+    <v-list-item v-for="note in recentNotes" 
+      :title="note.title" 
+      :subtitle="timeAgo.format(new Date(parseInt(note.lastEdited)), 'mini')" 
+      :to="`/note/${note.id}`">
+    </v-list-item>
   </v-list>
 </template>

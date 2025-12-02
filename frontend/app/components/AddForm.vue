78a407475312032881c81c62f76031df36e68cae
Glynn Bird <glynn.bird@gmail.com>
2025-12-02 10:51:13 +0000

    colour picker

--- frontend/app/components/AddForm.vue
+++ frontend/app/components/AddForm.vue
@@ -2,10 +2,15 @@
 // incoming programme to add/edit, whether the page is busy ot not and the submit button title
 const { doc} = defineProps(['doc'])
 
+const colours = [ ['#B71C1C', '#880E4F', '#4A148C', '#311B92', '#1A237E', '#0D47A1', '#01579B', '#006064'], ['#004D40', '#1B5E20', '#33691E', '#827717', '#F57F17',
+    '#FF6F00', '#E65100', '#BF360C'], ['#3E2723', '#263238','#E53935', '#D81B60', '#8E24AA', '#7E57C2', '#5C6BC0', '#42A5F5'], ['#039BE5', '#00ACC1', '#00897B', '#66BB6A',
+  '#7CB342', '#C0CA33', '#FDD835', '#FFB300'], ['#FB8C00', '#F4511E', '#6D4C41', '#546E7A', '#757575']]
+
 </script>
 <template>
   <v-form>
     <v-text-field v-model="doc.title" label="Title" required autofocus></v-text-field>
+    <v-color-picker swatches-max-height="200" height="200" :swatches="colours" class="ma-2" show-swatches v-model="doc.colour" hide-canvas hide-eye-dropper hide-header hide-inputs hide-sliders hide-title></v-color-picker>
     <v-textarea v-model="doc.body" label="Body" style="height: 600px"></v-textarea>
   </v-form>
 </template>
--- frontend/app/pages/edit/[id].vue
+++ frontend/app/pages/edit/[id].vue
@@ -5,19 +5,8 @@
 
   getNoteFromCache(id)
 
-  function pickColour() {
-      const colours = [ '#B71C1C', '#880E4F', '#4A148C', '#311B92', '#1A237E', '#0D47A1', '#01579B', '#006064', '#004D40', '#1B5E20', '#33691E', '#827717', '#F57F17',
-    '#FF6F00', '#E65100', '#BF360C', '#3E2723', '#263238','#E53935', '#D81B60', '#8E24AA', '#7E57C2', '#5C6BC0', '#42A5F5', '#039BE5', '#00ACC1', '#00897B', '#66BB6A',
-  '#7CB342', '#C0CA33', '#FDD835', '#FFB300', '#FB8C00', '#F4511E', '#6D4C41', '#546E7A', '#757575']
-      const r = Math.floor(Math.random() * colours.length)
-      currentNote.value.colour = colours[r]
-  }
-
   setTimeout(async function() {
     await getNoteFromAPI(id)
-    if (typeof currentNote.value.colour === 'undefined') {
-      pickColour()
-    }
   }, 1)
   
   async function edit() {
@@ -34,15 +23,7 @@
 </script>
 <template>
   <v-card>
-    <v-card-title>Edit
-      <v-btn
-        size="xx-small"
-        aria-label="Refresh"
-        icon="mdi-refresh"
-        :color="currentNote.colour"
-        @click="pickColour()"
-      ></v-btn>
-    </v-card-title>
+    <v-card-title>Edit</v-card-title>
     <v-card-text>
       <AddForm v-if="currentNote != null" :doc="currentNote"></AddForm>
     </v-card-text>

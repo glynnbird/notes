2868784a7dff0b656470e2df8303b131222ed625
Glynn Bird <glynn.bird@gmail.com>
2026-07-10 11:22:48 +0100

    app bar optional

--- frontend/app/layouts/minimal.vue
+++ frontend/app/layouts/minimal.vue
@@ -3,9 +3,9 @@
 </script>
 <template>
   <v-app theme="light">
-    <v-app-bar density="compact" color="#ffffff" flat>
+    <v-app-bar density="compact" color="#ffffff" flat v-if="route.name !== 'ro'">
       <template v-slot:append>
-       <v-btn v-if="route.name !== 'ro'"icon="mdi-arrow-left" @click="$router.back()"></v-btn>
+       <v-btn  icon="mdi-arrow-left" @click="$router.back()"></v-btn>
       </template>
     </v-app-bar>
     <v-main>
